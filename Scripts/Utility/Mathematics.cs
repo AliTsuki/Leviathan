573f964693e0e6c33419bb3ddbda1922421c30cf
Ali M <PhotonPhighter@gmail.com>
2019-07-11 19:38:34 -0600

    0.0.14e
    Added float2 class

--- Scripts/Utility/Mathematics.cs
+++ Scripts/Utility/Mathematics.cs
@@ -8,3 +8,26 @@ public static class Mathematics
         return (float)(Math.Round(_input * 2, MidpointRounding.AwayFromZero) / 2);
     }
 }
+
+public class Float2
+{
+    public float x { get; set; }
+    public float y { get; set; }
+
+    public Float2()
+    {
+        this.x = 0f;
+        this.y = 0f;
+    }
+
+    public Float2(float _x, float _y)
+    {
+        this.x = _x;
+        this.y = _y;
+    }
+
+    public override string ToString()
+    {
+        return $@"({this.x}, {this.y})";
+    }
+}
